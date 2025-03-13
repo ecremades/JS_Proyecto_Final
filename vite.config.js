1a92ebf1b15a685a0dcb31113b0e3ef9c6a69c14
ecremades <ecremades86@hotmail.com>
2025-03-13 06:30:23 +0100

    Configuración inicial del proyecto: archivos de configuración y dependencias

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [],
+  base: '/JS_Proyecto_Final/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+  },
+});
